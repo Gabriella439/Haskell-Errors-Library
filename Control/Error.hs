4338496017fe91ff29d160105ad14a916816d063
Gabriel Gonzalez <Gabriel439@gmail.com>
2012-07-07 13:25:57 -0700

    Added several additional re-exports to Control.Error

--- Control/Error.hs
+++ Control/Error.hs
@@ -4,20 +4,28 @@
 
 > import Control.Error
 
-    This module exports the entire library as well as functions from the @Safe@
-    library.
+    This module exports the entire library as well as useful exports from other
+    standard error-handling libraries.
 -}
 
 module Control.Error (
     module Control.Error.Script,
     module Control.Error.Safe,
     module Control.Error.Util,
+    module Control.Monad.Trans.Either,
+    module Control.Monad.Trans.Maybe,
+    module Data.Either,
     module Data.EitherR,
+    module Data.Maybe,
     module Safe
     ) where
 
 import Control.Error.Script
 import Control.Error.Safe
 import Control.Error.Util
+import Control.Monad.Trans.Either
+import Control.Monad.Trans.Maybe
+import Data.Either
 import Data.EitherR
+import Data.Maybe
 import Safe
