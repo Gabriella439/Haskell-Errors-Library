6ccf61ce8e26bfcbe897a9b0685427f55ff078b9
Toralf Wittner <tw@dtex.org>
2013-06-21 18:43:10 -0700

    Torian's patch

--- Control/Error/Util.hs
+++ Control/Error/Util.hs
@@ -8,6 +8,8 @@ module Control.Error.Util (
     note,
     noteT,
     hoistMaybe,
+    (?),
+    (!?),
     -- * MaybeT
     maybeT,
     just,
@@ -22,14 +24,18 @@ module Control.Error.Util (
     err,
     errLn,
     -- * Exceptions
-    tryIO
+    tryIO,
+    attempt
     ) where
 
-import Control.Exception (try, IOException)
+import Control.Applicative
+import Control.Exception
 import Control.Monad (liftM)
 import Control.Monad.IO.Class (MonadIO(liftIO))
 import Control.Monad.Trans.Either (EitherT(EitherT, runEitherT))
 import Control.Monad.Trans.Maybe (MaybeT(MaybeT, runMaybeT))
+import Data.Dynamic (Dynamic)
+import System.Exit (ExitCode)
 import System.IO (hPutStr, hPutStrLn, stderr)
 
 -- For Documentation
@@ -60,6 +66,14 @@ noteT a = EitherT . liftM (note a) . runMaybeT
 -- | Lift a 'Maybe' to the 'MaybeT' monad
 hoistMaybe :: (Monad m) => Maybe b -> MaybeT m b
 hoistMaybe = MaybeT . return
+
+-- | Convert a 'Maybe' value into the 'EitherT' monad.
+(?) :: Applicative m => Maybe a -> e -> EitherT e m a
+(?) a e = EitherT (pure $ note e a)
+
+-- | Convert an applicative 'Maybe' value into the 'EitherT' monad.
+(!?) :: Applicative m => m (Maybe a) -> e -> EitherT e m a
+(!?) a e = EitherT (note e <$> a)
 
 {-| Case analysis for 'MaybeT'
 
@@ -104,3 +118,64 @@ errLn = hPutStrLn stderr
 -- | Catch 'IOException's and convert them to the 'EitherT' monad
 tryIO :: (MonadIO m) => IO a -> EitherT IOException m a
 tryIO = EitherT . liftIO . try
+
+-- | Execute the given IO action, catch all exceptions except:
+--
+--     * 'AsyncException'
+--
+--     * 'ArithException'
+--
+--     * 'ErrorCall'
+--
+--     * 'Dynamic'
+--
+--     * 'ExitCode'
+--
+--     * 'ArrayException'
+--
+--     * 'AsyncException'
+--
+--     * 'AssertionFailed'
+--
+--     * 'Deadlock'
+--
+--     * 'BlockedIndefinitelyOnSTM'
+--
+--     * 'BlockedIndefinitelyOnMVar'
+--
+--     * 'NestedAtomically'
+--
+--     * 'NonTermination'
+--
+--     * 'NoMethodError'
+--
+--     * 'RecUpdError'
+--
+--     * 'RecConError'
+--
+--     * 'RecSelError'
+--
+--     * 'PatternMatchFail'
+--
+-- and convert them to the 'EitherT' monad.
+attempt :: MonadIO m => IO a -> EitherT SomeException m a
+attempt a = EitherT . liftIO $ catches (Right <$> a)
+    [ Handler $ \e -> throw (e :: ArithException)
+    , Handler $ \e -> throw (e :: ArrayException)
+    , Handler $ \e -> throw (e :: AssertionFailed)
+    , Handler $ \e -> throw (e :: AsyncException)
+    , Handler $ \e -> throw (e :: BlockedIndefinitelyOnMVar)
+    , Handler $ \e -> throw (e :: BlockedIndefinitelyOnSTM)
+    , Handler $ \e -> throw (e :: Deadlock)
+    , Handler $ \e -> throw (e :: Dynamic)
+    , Handler $ \e -> throw (e :: ErrorCall)
+    , Handler $ \e -> throw (e :: ExitCode)
+    , Handler $ \e -> throw (e :: NestedAtomically)
+    , Handler $ \e -> throw (e :: NoMethodError)
+    , Handler $ \e -> throw (e :: NonTermination)
+    , Handler $ \e -> throw (e :: PatternMatchFail)
+    , Handler $ \e -> throw (e :: RecConError)
+    , Handler $ \e -> throw (e :: RecSelError)
+    , Handler $ \e -> throw (e :: RecUpdError)
+    , Handler $ return . Left
+    ]
