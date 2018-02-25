de13c976f3a2055c2ff3fff2cbd66296028f9333
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-02-25 07:56:41 -0800

    Add Nix derivation to build project

---
+++ release.nix
@@ -0,0 +1,17 @@
+let
+  config = {
+    packageOverrides = pkgs: {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: {
+          errors = haskellPackagesNew.callPackage ./default.nix { };
+        };
+      };
+    };
+  };
+
+  pkgs =
+    import <nixpkgs> { inherit config; };
+
+in
+  { inherit (pkgs.haskellPackages) errors;
+  }
